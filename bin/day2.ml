53565bea37c2576cc5e922d9f6fcface31c4280e
Juergen Hoetzel <juergen@hoetzel.info>
2025-12-03 02:10:05 +0100

    Add Day 2: Part 2

--- bin/day2.ml
+++ bin/day2.ml
@@ -1,21 +1,32 @@
-let invalid x = Printf.sprintf "%d%d" x x |> int_of_string
+module IntSet = Set.Make (Int)
+
+let invalid x n =
+  let s = string_of_int x in
+  List.init n (Fun.const s) |> String.concat "" |> int_of_string
 
 let pow base exp =
   let rec loop acc = function 0 -> acc | n -> loop (acc * base) (n - 1) in
   loop 1 exp
 
-let invalid_in_range x y =
-  let e = (Float.log10 (float_of_int x) |> Float.ceil |> Int.of_float) + 1 in
-  let x_scan = x / pow 10 (e / 2) in
+let invalid_in_range replicate_multiple x y =
   let e = Float.log10 (float_of_int y) |> Float.ceil |> Int.of_float in
   let y_scan = y / pow 10 (e / 2) in
   let rec loop acc i =
     if i > y_scan then acc
     else
-      let iv = invalid i in
-      if x <= iv && iv <= y then loop (iv :: acc) (i + 1) else loop acc (i + 1)
+      let rec invalid_loop (invalids : IntSet.t) n =
+        let iv = invalid i n in
+        let new_invalids =
+          if x <= iv && iv <= y then IntSet.add iv invalids else invalids
+        in
+        if replicate_multiple then
+          if iv <= y then invalid_loop new_invalids (n + 1) else invalids
+        else new_invalids
+      in
+      let ivs = invalid_loop IntSet.empty 2 in
+      loop (IntSet.union acc ivs) (i + 1)
   in
-  loop [] x_scan
+  loop IntSet.empty 1 |> IntSet.to_list
 
 let parse s =
   List.filter_map
@@ -28,7 +39,11 @@ let parse s =
 let () =
   let ranges = In_channel.input_all stdin |> String.trim |> parse in
   let n =
-    List.map (fun (x, y) -> invalid_in_range x y) ranges
+    List.map (fun (x, y) -> invalid_in_range false x y) ranges
     |> List.concat |> List.fold_left ( + ) 0
   in
-  Printf.printf "%d\n%!" n
+  let n2 =
+    List.map (fun (x, y) -> invalid_in_range true x y) ranges
+    |> List.concat |> List.fold_left ( + ) 0
+  in
+  Printf.printf "%d\n%d\n%!" n n2
